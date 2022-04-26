987eb8b2d7dedb5531917ceb5e35c78f72fbd735
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-04-25 20:56:35 -0400

    lisa: configs: Import lahaina msm_irqbalance.conf
    Change-Id: I5a5e6c7770010b1e4e5aa54c3f95a8a20aa2c2f7

--- device.mk
+++ device.mk
@@ -333,6 +333,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # QMI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
