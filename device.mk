d693785ea23ce9f099158c6d19b18288f2110658
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-04-25 20:56:35 -0400

    lisa: configs: Import lahaina msm_irqbalance.conf
    Change-Id: I5a5e6c7770010b1e4e5aa54c3f95a8a20aa2c2f7

--- device.mk
+++ device.mk
@@ -392,6 +392,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # QTI
