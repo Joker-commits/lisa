ef7aa9045d4798083d18bd678824adc598f707a3
AdarshGrewal <adarshgrewal@gmail.com>
2022-05-24 00:08:55 +0530

    lisa: Build Neural Networks vendor hidl

--- device.mk
+++ device.mk
@@ -304,6 +304,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
+# Neural networks
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.3.vendor
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc_snxxx@1.2-service \
