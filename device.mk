90f763040cbbaf942ae22f030173af69f1770f27
Volodymyr Zhdanov <wight554@gmail.com>
2022-05-27 08:04:20 +0530

    lisa: Disable configstore
     * it's finally deprecated in R
     * ship disable_configstore because we have API level 29
    
    Change-Id: Ida4ae417bb0d5a22ea7eb9500e687a656be6a0ce

--- device.mk
+++ device.mk
@@ -179,6 +179,9 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.15.vendor \
     vendor.display.config@2.0.vendor
 
+PRODUCT_PACKAGES += \
+    disable_configstore
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/display_id_4630946736638489730.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946736638489730.xml \
     $(LOCAL_PATH)/configs/display/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml
