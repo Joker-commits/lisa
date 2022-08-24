f1b06f10fec124a7f3bb0d3ec5f472f68634709f
ALKALiKong <alikongalk@gmail.com>
2022-08-24 08:05:23 +0800

    lisa: ArrowOS bringup

--- arrow_lisa.mk
+++ arrow_lisa.mk
@@ -11,12 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lisa device
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
-# Inherit some common Kaleidoscope stuff
-$(call inherit-product, vendor/kscope/target/product/mobile.mk)
+# Inherit some common Arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := kscope_lisa
+PRODUCT_NAME := arrow_lisa
 PRODUCT_DEVICE := lisa
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Xiaomi 11 Lite NE
--- device.mk
+++ device.mk
@@ -391,7 +391,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-kscope
+    $(LOCAL_PATH)/overlay-arrow
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
