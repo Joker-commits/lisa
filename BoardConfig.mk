77d826e0818ccc9c676729b3f5023961ffcbd116
M3zaque <mezackisilva@gmail.com>
2023-03-15 10:44:42 -0300

    lisa: sepolicy: Include device qcom SEPolicy.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -161,6 +161,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USES_MKE2FS := true
 
+# Sepolicy
+include device/qcom/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
