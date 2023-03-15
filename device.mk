be6920abdda1defd06e42708daa7ea3694d783f6
AdarshGrewal <adarshgrewal@gmail.com>
2023-03-15 11:12:01 -0300

    lisa: Build 1.3 DRM clearkey service

--- device.mk
+++ device.mk
@@ -130,6 +130,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.3-service.clearkey
+
 # Fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
