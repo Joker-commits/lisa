86fd47b12c89eac9fb5802e503b2194d6348568d
M3zaque <mezackisilva@gmail.com>
2023-03-15 19:16:38 -0300

    lisa: Import stock wifi configs

--- device.mk
+++ device.mk
@@ -202,6 +202,14 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # Media
 PRODUCT_PACKAGES += \
     libcodec2_hidl@1.0.vendor \
