073caf61b67c3c44e1c1f39ae1a921897f30754b
M3zaque <mezackisilva@gmail.com>
2023-03-15 19:32:45 -0300

    lisa: overlay: Initial overlay * from  https://github.com/LineageOS/android_device_xiaomi_sm8350-common/commits/lineage-19.1/overlay

--- device.mk
+++ device.mk
@@ -250,6 +250,12 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
