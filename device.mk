db34476e387c45a9f5aad4ae66ee22e00691cda0
Dmitrii <bankersenator@gmail.com>
2023-04-18 12:46:10 -0300

    lisa: overlay: set voltageos maintainer string  - Add maintainer overlay  - Needed for VoltageOS settings to show maintainer name else will show as unknown maintainer
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- device.mk
+++ device.mk
@@ -401,7 +401,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-voltage
 
 PRODUCT_PACKAGES += \
     AOSPLisaCSOTFrameworksOverlay
