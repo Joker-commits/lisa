7889e04d7540b6740f2b4c921e0f4387e1599fc0
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2023-11-30 10:13:03 +0100

    lisa: Patch displayfeature.default.so to use v33 libstagefright_foundation
     * this fixes a fatal crash within displayfeature that prevented it from starting
    
    Change-Id: Ic102dc1fb5b27a849b55253d8b81828efd15c1fa

--- extract-files.sh
+++ extract-files.sh
@@ -98,7 +98,7 @@ function blob_fixup() {
             ;;
         vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefright_soft_ac4dec.so | \
         vendor/lib/libstagefrightdolby.so | vendor/lib64/libstagefright_soft_ddpdec.so | \
-        vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefrightdolby.so)
+        vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefrightdolby.so | vendor/lib64/hw/displayfeature.default.so)
             ${PATCHELF} --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
             ;;
     esac
