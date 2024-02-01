65cf57d0c228207f2708e02dd6b74725eb9b80d3
adrian1234a <adri16072002@gmail.com>
2024-02-01 23:30:12 +0000

    lisa: Set framework_compatibility for Lineage Health
    Change-Id: I966a656437635c60cd91c3221092b5c8411a46f8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,7 +86,8 @@ DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/vendor_framework_compatibility_matrix.xml \
-    $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml
+    $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/configs/hidl/crdroid_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/configs/hidl/manifest_lahaina.xml \
